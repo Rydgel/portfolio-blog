f239b873938913df8f73e53266e0618b3022ad10
Jérôme Mahuet <jerome.mahuet@gmail.com>
2020-11-14 16:22:53 +0100

    typekit

---
+++ src/components/third-party/typekit.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+type TypeScriptData = {
+    kitId: string;
+};
+
+const TypekitScript: FC<TypeScriptData> = (props: TypeScriptData) => {
+    return (
+        <script>
+            {`
+      (function(d) {
+          var config = {
+              kitId: '${props.kitId}',
+              scriptTimeout: 3000,
+              async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    `}
+        </script>
+    );
+};
+
+const Typekit: FC<TypeScriptData> = (props: TypeScriptData) => {
+    return <Head>{TypekitScript(props)}</Head>;
+};
+
+export default Typekit;
