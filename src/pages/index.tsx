f85f47110fdd3802cf7de8f117a8d563ca13dfe1
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-02-07 09:44:44 +0100

    change fonts in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,10 +34,10 @@ function onePostRow(type: string): (post: Post | Experiment) => React.ReactNode 
     return function (post: Post | Experiment): React.ReactNode {
         const PostRow = () => (
             <div key={post.id}>
-                <h3 className="text-2xl font-medium">
+                <h3 className="text-3xl font-medium">
                     <Link href={`/${type}/${post.slug}`}>{post.title}</Link>
                 </h3>
-                <p className="text-gray-500">{formatDate(post.display_time)}</p>
+                <p className="text-gray-400 italic">{formatDate(post.display_time)}</p>
                 <div className="prose lg:prose-xl">
                     <ReactMarkdown>{post.description}</ReactMarkdown>
                 </div>
