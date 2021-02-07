e20702d96825dab98fbb23fe7fa8cb84aef78cfe
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-02-07 14:00:33 +0100

    footer

--- src/components/design/footer.tsx
+++ src/components/design/footer.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
 
 const Footer: FC = () => {
+    const currentYear = new Date().getFullYear();
     return (
-        <footer className="text-base mt-4"></footer>
+        <footer className="text-base mt-16 dark:text-gray-600">
+            <div className="max-w-5xl text-center m-auto">
+                <p>Made in France in {currentYear} with NextJS, Typescript, Strapi, Tailwind etc.</p>
+            </div>
+        </footer>
     );
 };
 
