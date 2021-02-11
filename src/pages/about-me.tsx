ccd59cfa8ffedcb6d79fb8e529e026ce7067bcb2
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-02-11 21:48:29 +0100

    about me page

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -18,40 +18,40 @@ const AboutMePage: FC<AboutMeProps> = (props: AboutMeProps) => {
                 <div>
                     <h2 className="text-5xl font-bold block text-center mb-14">About me</h2>
                     <div className="prose lg:prose-xl dark:prose-dark dark:lg:prose-dark-xl">
-                        <p>
+                        <p className="text-center">
                             Web <em>&amp;</em> mobile developer with a soft spot for typography and functional
                             programming. I’ve been working in the field for 15 years and I’m still looking to improve
                             and use the latest technologies for the job. I’m also interested in 3D rendering and the
                             making of video games. My favorite languages right now are{' '}
                             <a href="https://www.rust-lang.org/">Rust</a>,{' '}
                             <a href="https://www.typescriptlang.org/">Typescript</a>,{' '}
-                            <a href="https://www.haskell.org/">Haskell</a> <em>&amp;</em>{' '}
+                            <a href="https://www.haskell.org/">Haskell</a> and{' '}
                             <a href="https://elixir-lang.org/">Elixir</a>.
                             <br />
                             Feel free to contact me.
                         </p>
-                        <h3>Find me on</h3>
+                        <h3 className="text-center">Find me on</h3>
                     </div>
                 </div>
                 <div className="mt-4">
                     <section className="text-base lg:text-xl">
-                        <ul>
+                        <ul className="text-center">
                             <li className="mb-2">
                                 <a href="mailto:jerome.mahuet@gmail.com" className="no-underline hover:underline">
                                     <SiMinutemailer className="inline-block mr-3" />
-                                    Email
+                                    jerome.mahuet@gmail.com
                                 </a>
                             </li>
                             <li className="mb-2">
-                                <a href="https://github.com/Rydgel" className="no-underline hover:underline">
+                                <a href="https://github.com/rydgel" className="no-underline hover:underline">
                                     <SiGithub className="inline-block mr-3" />
-                                    GitHub
+                                    https://github.com/rydgel
                                 </a>
                             </li>
                             <li className="mb-2">
                                 <a href="https://twitter.com/rydgel" className="no-underline hover:underline">
                                     <SiTwitter className="inline-block mr-3" />
-                                    Twitter
+                                    https://twitter.com/rydgel
                                 </a>
                             </li>
                         </ul>
