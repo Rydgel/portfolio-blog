02ec343a3aab25a87aaa42869e6488ee91fc5633
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-02-15 15:28:55 +0100

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,15 @@ import React, { FC } from 'react';
 import Layout from '../components/design/layout';
 
 const NotFoundPage: FC = () => {
-    return <Layout page="index">
-        <p className="inline-block w-full text-9xl font-bold text-center leading-normal">
-            404
-            <br/>
-            Not Found
-        </p>
-    </Layout>;
+    return (
+        <Layout page="index">
+            <p className="inline-block w-full text-9xl font-bold text-center leading-normal">
+                404
+                <br />
+                Not Found
+            </p>
+        </Layout>
+    );
 };
 
 export default NotFoundPage;
