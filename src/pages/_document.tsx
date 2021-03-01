6f748b5f40f34208867bfbd60ed461aca06d7ee2
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-03-01 16:26:46 +0100

    fuck

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,32 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { getInitialTheme } from '../components/theme/themeContext';
 
 function setInitialColorMode() {
-    const rawTheme = getInitialTheme();
+    function getInitialColorMode() {
+        const preference = window.localStorage.getItem('color-theme');
+        let hasPreference = typeof preference === 'string';
+
+        /**
+         * If the user has explicitly chosen light or dark,
+         * use it. Otherwise, this value will be null.
+         */
+        if (hasPreference) {
+            return preference;
+        }
+
+        // If there is no saved preference, use a media query
+        const mediaQuery = '(prefers-color-scheme: dark)';
+        const mql = window.matchMedia(mediaQuery);
+
+        hasPreference = typeof mql.matches === 'boolean';
+        if (hasPreference) {
+            return mql.matches ? 'dark' : 'light';
+        }
+
+        // default to 'light'.
+        return 'light';
+    }
+
+    const rawTheme = getInitialColorMode();
 
     const root = window.document.documentElement;
     const isDark = rawTheme === 'dark';
