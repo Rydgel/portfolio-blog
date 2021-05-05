02389379170c6fc5c0a3976dc1c6ea075881c631
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-05-05 13:56:10 +0200

    retry with light

--- src/lib/renderer.tsx
+++ src/lib/renderer.tsx
@@ -1,7 +1,22 @@
 import { Component } from 'react';
 
-import { default as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { hybrid as cstyle } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
+// only import used languages for reduced footprint
+import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
+import ts from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
+import bash from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash';
+import cpp from 'react-syntax-highlighter/dist/cjs/languages/hljs/cpp';
+import haskell from 'react-syntax-highlighter/dist/cjs/languages/hljs/haskell';
+import rust from 'react-syntax-highlighter/dist/cjs/languages/hljs/rust';
+
+SyntaxHighlighter.registerLanguage('javascript', js);
+SyntaxHighlighter.registerLanguage('typescript', ts);
+SyntaxHighlighter.registerLanguage('bash', bash);
+SyntaxHighlighter.registerLanguage('cpp', cpp);
+SyntaxHighlighter.registerLanguage('haskell', haskell);
+SyntaxHighlighter.registerLanguage('rust', rust);
 
 type SyntaxProps = {
     language?: string;
