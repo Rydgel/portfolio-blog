efafb90337f6a4c8beb79605ab93356e98781009
Jérôme Mahuet <jerome.mahuet@gmail.com>
2021-05-05 13:40:57 +0200

    revert shit ass lib

--- src/lib/renderer.tsx
+++ src/lib/renderer.tsx
@@ -1,15 +1,7 @@
 import { Component } from 'react';
 
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { default as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { hybrid as cstyle } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-
-// only import used languages for reduced footprint
-import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
-import ts from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash';
-import cpp from 'react-syntax-highlighter/dist/cjs/languages/hljs/cpp';
-import haskell from 'react-syntax-highlighter/dist/cjs/languages/hljs/haskell';
-import rust from 'react-syntax-highlighter/dist/cjs/languages/hljs/rust';
 
 type SyntaxProps = {
     language?: string;
@@ -17,15 +9,6 @@ type SyntaxProps = {
 };
 
 class MySyntax extends Component<SyntaxProps> {
-    componentDidMount() {
-        SyntaxHighlighter.registerLanguage('javascript', js);
-        SyntaxHighlighter.registerLanguage('typescript', ts);
-        SyntaxHighlighter.registerLanguage('bash', bash);
-        SyntaxHighlighter.registerLanguage('cpp', cpp);
-        SyntaxHighlighter.registerLanguage('haskell', haskell);
-        SyntaxHighlighter.registerLanguage('rust', rust);
-    }
-
     render(): JSX.Element {
         return (
             <SyntaxHighlighter style={cstyle} language={this.props.language} wrapLongLines={true}>
