87761c98b24e9b63a75c2d79cf030a5c20375493
Jérôme Mahuet <jerome.mahuet@gmail.com>
2022-08-04 13:45:49 +0200

    fix hydratation errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    reactStrictMode: true,
     images: {
         domains: ['api.jeromem.dev'],
     },
--- src/lib/renderer.tsx
+++ src/lib/renderer.tsx
@@ -1,32 +1,18 @@
-import type { CodeProps } from 'react-markdown/lib/ast-to-react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { hybrid as cstyle } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-
-// only import used languages for reduced footprint
-import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
-import ts from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash';
-import cpp from 'react-syntax-highlighter/dist/cjs/languages/hljs/cpp';
-import haskell from 'react-syntax-highlighter/dist/cjs/languages/hljs/haskell';
-import rust from 'react-syntax-highlighter/dist/cjs/languages/hljs/rust';
-
-SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('typescript', ts);
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('cpp', cpp);
-SyntaxHighlighter.registerLanguage('haskell', haskell);
-SyntaxHighlighter.registerLanguage('rust', rust);
-
-const renderCodeSyntax = ({ inline, className, children }: CodeProps): JSX.Element => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const renderCodeSyntax = ({ node, inline, className, children, ...props }): JSX.Element => {
     const match = /language-(\w+)/.exec(className || '');
     const content = String(children).replace(/\n$/, '');
     return !inline && match ? (
-        <SyntaxHighlighter style={cstyle} language={match[1]} wrapLongLines={true}>
+        <SyntaxHighlighter style={nord} language={match[1]} wrapLongLines={true} PreTag="div" {...props}>
             {content}
         </SyntaxHighlighter>
     ) : (
-        <code className={className}>{content}</code>
+        <code className={className} {...props}>
+            {content}
+        </code>
     );
 };
 
